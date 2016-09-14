88f77332ccedab2c126d1e2ec766b573808ede87
ring04h <ring04h@gmail.com>
2016-09-14 11:57:20 +0800

    health check
    add new

---
+++ tools/health_check.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+health check tool, 
+if wyproxy is error, then restart the proxy server.
+$ python wyproxy.py -restart
+: wyporyx.py need add -conf options
+"""
+
