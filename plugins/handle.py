48817f0b5da044b5f8903215995591574a449996
ring04h <rring04h@gmail.com>
2016-12-29 15:26:44 +0800

    format
    format

--- plugins/handle.py
+++ plugins/handle.py
@@ -5,18 +5,9 @@
 def wyproxy_request_handle(flow):
     """wyproxy send data to server before processing"""
 
-<<<<<<< HEAD
     flow.request.anticache()  # disable cache
     flow.request.anticomp()   # disable gzip compress
-    
-=======
-    # disable cache
-    flow.request.anticache()
 
-    # disable gzip compress
-    flow.request.anticomp()
-
->>>>>>> origin/master
     # change the request headers['Host']
     # flow.request.headers['X-Online-Host'] = 'wap.gd.10086.cn'
 
